83dde1990f2b40f48cdc1c93278fa6935185e6d3
AlistairHopkins92 <alistairhopkins92@gmail.com>
2017-04-19 13:20:49 +0100

    Set up of the main page showing content

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,11 @@
-// let's go!
+import React from 'react';
+
+import {render} from 'react-dom';
+
+//CSS
+import CSS from './styles/style.styl';
+
+//Components
+import Main from './components/Main';
+
+render(<Main />, document.getElementById('root'));
